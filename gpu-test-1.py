c1c92656001398ee404e7a2fc30cdc171983315d
krudisar <krudisar@gmail.com>
2020-03-05 13:15:01 +0100

    Create gpu-test-1.py

---
+++ gpu-test-1.py
@@ -0,0 +1,8 @@
+## Libraries import
+import tensorflow as tf
+## Test GPU
+device_name = tf.test.gpu_device_name()
+if device_name != '/device:GPU:0':
+  raise SystemError('GPU device not found')
+print('Found GPU at: {}'.format(device_name))
+print('')
