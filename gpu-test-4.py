f4205e5e564d7a963b1f7a3bd13caa242b6e6850
krudisar <krudisar@gmail.com>
2020-03-07 21:57:04 +0100

    another commit

--- gpu-test-4.py
+++ gpu-test-4.py
@@ -1,3 +1,16 @@
+# pip install gpuinfo
 from gpuinfo import GPUInfo
 
+available_device=GPUInfo.check_empty()
+percent,memory=GPUInfo.gpu_usage()
+min_percent=percent.index(min([percent[i] for i in available_device]))
+min_memory=memory.index(min([memory[i] for i in available_device]))
+
 print(GPUInfo.get_info())
+
+print(".\n.\n.")
+
+print("Available devices: " + available_device + "\n\n\n")
+print("CPU Usage: " + percent)
+print("Available memory : " + memory)
+
