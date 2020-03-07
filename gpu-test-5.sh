3ca8dc34f8d62e992e1c01e7b9628edfde335a42
krudisar <krudisar@gmail.com>
2020-03-07 15:29:44 +0100

    Create gpu-test-5.sh

---
+++ gpu-test-5.sh
@@ -0,0 +1,11 @@
+echo "Running nvidia-debugdump command ..."
+/usr/local/nvidia/bin/nvidia-debugdump -l -v
+
+echo ""
+echo "."
+echo "."
+echo "."
+echo ""
+
+echo "Running nvidia-smi command ..."
+/usr/local/nvidia/bin/nvidia-smi
