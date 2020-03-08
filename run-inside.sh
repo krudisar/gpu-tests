66a95d69d455026dcc23d9548c4c51c2b02fe255
krudisar <krudisar@gmail.com>
2020-03-08 23:41:05 +0100

    Update run-inside.sh

--- run-inside.sh
+++ run-inside.sh
@@ -3,3 +3,9 @@
 # this is meant to be executed inside Kubernetes pod or job
 # 
 
+# download the ML TensorFlow sample script
+wget https://raw.githubusercontent.com/krudisar/gpu-tests/master/tf-test.py
+
+# install Python3 module to access NVIDIA GPU and check the status
+pip3 install pynvml
+
