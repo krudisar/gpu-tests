d64d5463a2c55ce0f2a1a3294cf46b8a4c2c9bf2
krudisar <krudisar@gmail.com>
2020-03-09 12:56:24 +0100

    update

--- tf-test-2.py
+++ tf-test-2.py
@@ -1,7 +1,7 @@
 # the Python code to check that TensorFlow job inside the container or K8S pod is really running on GPU
 
 import tensorflow as tf
-with tf.device('/xla_gpu:0'):
+with tf.device('/gpu:0'):
     a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
     b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
     c = tf.matmul(a, b)
