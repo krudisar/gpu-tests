6ff7bc201087ade08e4e2932bbeeba1fb138aec4
krudisar <krudisar@gmail.com>
2020-03-09 13:36:35 +0100

    update

--- gpu-test-1.py
+++ gpu-test-1.py
@@ -1,3 +1,7 @@
+# supress all TensorFlow debug information
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}
+#
 ## Libraries import
 import tensorflow as tf
 ## Test GPU
