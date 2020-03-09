d52385a2028dbb6c05cf09aee50aa0c892148f48
krudisar <krudisar@gmail.com>
2020-03-09 13:42:38 +0100

    update

--- tf-test-2.py
+++ tf-test-2.py
@@ -1,3 +1,8 @@
+# supress all TensorFlow debug information
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}
+#
+
 # the Python code to check that TensorFlow job inside the container or K8S pod is really running on GPU
 
 import tensorflow as tf
@@ -6,6 +11,6 @@ with tf.device('/gpu:0'):
     b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
     c = tf.matmul(a, b)
 
-with tf.Session() as sess:
+with tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(log_device_placement=True)) as sess:
     print (sess.run(c))
     
